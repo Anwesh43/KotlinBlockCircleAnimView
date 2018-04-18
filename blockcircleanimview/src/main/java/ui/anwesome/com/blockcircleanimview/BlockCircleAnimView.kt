05d0582df36590e383c2cd941f4c5736fd0fd217
Anwesh43 <anweshthecool0@gmail.com>
2018-04-18 07:49:45 +0530

    adding blockcircleanimview

--- blockcircleanimview/src/main/java/ui/anwesome/com/blockcircleanimview/BlockCircleAnimView.kt
+++ blockcircleanimview/src/main/java/ui/anwesome/com/blockcircleanimview/BlockCircleAnimView.kt
@@ -3,3 +3,25 @@ package ui.anwesome.com.blockcircleanimview
 /**
  * Created by anweshmishra on 18/04/18.
  */
+
+import android.content.*
+import android.graphics.*
+import android.view.*
+
+class BlockCircleAnimView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
